13a4c4d3c55116ec3469d149ef6d05b05e2a7ff0
Skoolife App <skoolife.co@gmail.com>
2025-09-10 22:49:11 +0200

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
+    // Optionnel mais utile si tu continues à voir un token TS non compris
+    plugins: [['@babel/plugin-transform-typescript', { isTSX: true }]],
   };
 };
